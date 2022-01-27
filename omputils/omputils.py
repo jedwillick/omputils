b818b1e7d9d40a3e6fa956541663fde9d660e083
JedWillick <jwillick@me.com>
2022-01-27 19:24:23 +1000

    Default and set are working

--- omputils/omputils.py
+++ omputils/omputils.py
@@ -5,9 +5,15 @@ import os
 import sys
 import glob
 import subprocess
+import re
 
-DEFAULT = "https://raw.githubusercontent.com/jedwillick/onedark-omp/main/onedark.omp.json"
+DEFAULT_URL = "raw.githubusercontent.com/jedwillick/onedark-omp/main/onedark.omp.json"
+DEFAULT_NAME = "onedark"
+
 OS = sys.platform
+if OS not in ["linux", "win32"]:
+    print("Unsupported OS :(")
+    sys.exit(1)
 
 
 def setup_argparse():
@@ -16,13 +22,14 @@ def setup_argparse():
     subparser = p_root.add_subparsers(metavar="MODE", required=True, dest='command')
     p_theme = subparser.add_parser("theme", help="Commands to alter the overall theme.")
     group_theme = p_theme.add_mutually_exclusive_group(required=True)
-    group_theme.add_argument("-s", "--set", metavar="NAME", nargs='?', const="onedark",
+    group_theme.add_argument("-s", "--set", metavar="NAME", nargs='?', const=DEFAULT_NAME,
                              help="Sets to the specified theme, or to the default.")
     group_theme.add_argument("-l", "--list", metavar='SEARCH', nargs='?',
                              help="Lists all the themes or only those that match the search term.")
     group_theme.add_argument("-g", "--get", metavar="URL", nargs='?',
                              help="Downloads a theme or the default into your theme path and then sets to it.")
-    group_theme.add_argument("-d", "--default", metavar="URL", help="Change the default theme")
+    group_theme.add_argument("-d", "--default", metavar="URL", help="Change the default theme URL and NAME")
+    group_theme.add_argument("-dn", "--default-name", metavar="NAME", help="Change the default theme NAME. e.g. space")
     group_theme.add_argument("-r", "--random", action='store_true')
 
     p_path = subparser.add_parser("path", help="Commands to alter the pathstlye.")
@@ -35,35 +42,47 @@ def setup_argparse():
 def handle_theme(args):
     POSH_THEME = os.environ.get("POSH_THEME")
     THEME_PATH = os.path.dirname(POSH_THEME)
-    print(POSH_THEME)
-    if args.default:
+
+    def set_theme(theme: str):
+        if glob.glob(f"{THEME_PATH}/{theme}.omp.json"):
+            if OS == "linux":
+                subprocess.call(["bash", "-c",
+                                f'sed -i "s|export POSH_THEME=.*|export POSH_THEME={THEME_PATH}/{theme}.omp.json|" ~/.bashrc'])
+            else:
+                subprocess.call(["pwsh", "-c",
+                                f"""(Get-Content $PROFILE).replace('$env:POSH_THEME = "{POSH_THEME}"', 
+                                    '$env:POSH_THEME = "{THEME_PATH}\\{theme}.omp.json"') | Set-Content $PROFILE"""])
+        else:
+            print(f"Invalid theme '{theme}'")
+            print("Try 'omputils theme --help for more information")
+            sys.exit(1)
+
+    if args.default or args.default_name:
         with open(__file__, 'r') as reader:
             lines = reader.readlines()
 
         for i, line in enumerate(lines):
-            if line.startswith("DEFAULT"):
-                lines[i] = f"DEFAULT = {args.default}"
+            if args.default and line.startswith("DEFAULT_URL"):
+                lines[i] = f"DEFAULT_URL = '{args.default}'\n"
+            elif line.startswith("DEFAULT_NAME"):
+                if args.default:
+                    args.default_name = re.findall(r'([^/]+).omp.json', args.default)[0]
+                lines[i] = f"DEFAULT_NAME = '{args.default_name}'\n"
                 break
 
         with open(__file__, "w") as writer:
             writer.writelines(lines)
 
+        set_theme(args.default_name)
+
     elif args.set:
-        if glob.glob(f"{THEME_PATH}/{args.set}.omp.json"):
-            if OS == "linux":
-                subprocess.call(["bash", "-c",
-                                 f'sed -i "s|export POSH_THEME=.*|export POSH_THEME={THEME_PATH}/{args.set}.omp.json|" ~/.bashrc'])
-            elif OS == "win32":
-                subprocess.call(["pwsh", "-c",
-                                 f"""(Get-Content $PROFILE).replace('$env:POSH_THEME = "{POSH_THEME}"', '$env:POSH_THEME = "{THEME_PATH}\\{args.set}.omp.json"') | Set-Content $PROFILE"""])
-        else:
-            print(f"omputils: invalid theme '{args.set}'")
-            print("Try 'omputils theme --help for more information")
-            sys.exit(1)
+        set_theme(args.set)
+    elif args.get:
+        pass
 
 
 def main():
     args = setup_argparse()
-    # print(vars(args))
+    print(vars(args))
     if args.command == "theme":
         handle_theme(args)
